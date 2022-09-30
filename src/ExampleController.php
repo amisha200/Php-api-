638a16eebaeec230f47810f971b0d665f024a644
Amish Bharti <47895005+amish1999@users.noreply.github.com>
2022-10-01 01:37:59 +0530

    updated file and issue fixed

--- src/ExampleController.php
+++ src/ExampleController.php
@@ -16,5 +16,18 @@ class ExampleController extends JsonController
     /**
      * {@inheritdoc}
      */
-   //  //
+   public function invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
+    {
+        // output the data that was received by various methods
+        $data = [
+            'GET' => $request->getQueryParams(),
+            'POST' => $request->getParsedBody(),
+            'args' => $args,
+        ];
+
+        return [
+            'status_code' => 200,
+            'data' => array_filter($data),
+        ];
+    }
 }
