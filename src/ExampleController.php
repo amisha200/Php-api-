9b8b54e30d98f9bc26cfd36a034f4185c456b7f7
Amisha <57953781+amisha200@users.noreply.github.com>
2022-10-01 01:24:29 +0530

    Update ExampleController.php

--- src/ExampleController.php
+++ src/ExampleController.php
@@ -16,18 +16,5 @@ class ExampleController extends JsonController
     /**
      * {@inheritdoc}
      */
-    public function invoke(ServerRequestInterface $request, ResponseInterface $response, array $args)
-    {
-        // output the data that was received by various methods
-        $data = [
-            'GET' => $request->getQueryParams(),
-            'POST' => $request->getParsedBody(),
-            'args' => $args,
-        ];
-
-        return [
-            'status_code' => 200,
-            'data' => array_filter($data),
-        ];
-    }
+   //  //
 }
